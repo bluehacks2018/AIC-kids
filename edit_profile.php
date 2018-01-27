08f997355933182696cd137991a03d7b0e22c1b3
favs <jfavorito@gbox.adnu.edu.ph>
2018-01-28 03:49:49 +0800

    temporary update

--- edit_profile.php
+++ edit_profile.php
@@ -1,3 +1,13 @@
+<?php
+  session_start();
+  $id = $_SESSION['id'];
+  $conn = mysqli_connect('127.0.0.1', 'root', '', 'juanjob');
+  if ($id < 1)
+    header('Location: index.php');
+  $query = "SELECT * FROM provider where provider_id = $id";
+  $result = mysqli_query($conn, $query);
+  $row = mysqli_fetch_assoc($result);
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -22,8 +32,7 @@
           <div class="form-group">
             <label class="col-md-12 control-label" for="last_name">Last Name</label>  
             <div class="col-md-12">
-            <input id="last_name" name="last_name" type="text" placeholder="Rizal" class="form-control input-md" required="">
-              
+            <input id="last_name" name="last_name" value="<?php echo $row["last_name"]; ?>" type="text" placeholder="Rizal" class="form-control input-md" required="">
             </div>
           </div>
 
@@ -31,8 +40,7 @@
           <div class="form-group">
             <label class="col-md-12 control-label" for="first_name">First Name</label>  
             <div class="col-md-12">
-            <input id="first_name" name="first_name" type="text" placeholder="Jose" class="form-control input-md" required="">
-              
+            <input id="first_name" name="first_name" value="<?php echo $row["first_name"]; ?>" type="text" placeholder="Jose" class="form-control input-md" required="">
             </div>
           </div>
 
@@ -40,8 +48,7 @@
           <div class="form-group">
             <label class="col-md-12 control-label" for="contact">Contact</label>  
             <div class="col-md-12">
-            <input id="contact" name="contact" type="text" placeholder="09999999999" class="form-control input-md" required="">
-              
+            <input id="contact" name="contact" type="text" value="<?php echo $row["contact"]; ?>" class="form-control input-md" required="">
             </div>
           </div>
 
@@ -49,7 +56,7 @@
           <div class="form-group">
             <label class="col-md-12 control-label" for="street">Street</label>  
             <div class="col-md-12">
-            <input id="street" name="street" type="text" placeholder="" class="form-control input-md" required="">
+            <input id="street" name="street" type="text" value="<?php echo $row["street"]; ?>" class="form-control input-md" required="">
               
             </div>
           </div>
@@ -58,7 +65,7 @@
           <div class="form-group">
             <label class="col-md-12 control-label" for="barangay">Barangay</label>  
             <div class="col-md-12">
-            <input id="barangay" name="barangay" type="text" placeholder="" class="form-control input-md">
+            <input id="barangay" name="barangay" value="<?php echo $row["brgy"]; ?>" type="text" placeholder="" class="form-control input-md">
               
             </div>
           </div>
@@ -67,7 +74,7 @@
           <div class="form-group">
             <label class="col-md-12 control-label" for="City">City</label>  
             <div class="col-md-12">
-            <input id="City" name="City" type="text" placeholder="" class="form-control input-md">
+            <input id="City" name="city" type="text" value="<?php echo $row["city"]; ?>" class="form-control input-md">
               
             </div>
           </div>
