a4b09c0ff06dc34fb144a065453d187e68fee6c7
jariola <jariola@gbox.adnu.edu.ph>
2018-01-28 00:48:52 +0800

    added jobs in registration

---
+++ footer.php
@@ -0,0 +1,11 @@
+<!-- <footer class="footer">
+  <div class="container">
+    <span class="text-muted">Place sticky footer content here.</span>
+  </div>
+</footer> -->
+
+<div class="copyright py-4 text-center text-white">
+  <div class="container">
+    <small>Copyright &copy; <?php include("name.php") ?> 2018</small>
+  </div>
+</div>
