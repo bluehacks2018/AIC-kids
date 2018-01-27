366fcc4e95ea3f9b541f1ad6575f0606da169dc7
jariola <jariola@gbox.adnu.edu.ph>
2018-01-28 05:01:00 +0800

    fixed navbar

--- navbar.php
+++ navbar.php
@@ -4,19 +4,21 @@
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <div class="collapse navbar-collapse text-uppercase" id="navbarsExample03">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="edit_profile.php">Edit Profile</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="account.php">account</a>
-      </li>
-    </ul>
+  <?php 
+  if (isset($_SESSION['id']))
+    echo '<div class="collapse navbar-collapse text-uppercase" id="navbarsExample03">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="edit_profile.php">Edit Profile</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="account.php">account</a>
+        </li>
+      </ul>
 
-    <form class="form-inline my-2 my-md-0" action="logout.php" method="post">
-      <button id="submit" name="submit" class="btn btn-danger">LOG OUT</button>
-    </form>
-  
-  </div>
+      <form class="form-inline my-2 my-md-0" action="logout.php" method="post">
+        <button id="submit" name="submit" class="btn btn-danger">LOG OUT</button>
+      </form>
+    </div>'
+   ?>
 </nav>
